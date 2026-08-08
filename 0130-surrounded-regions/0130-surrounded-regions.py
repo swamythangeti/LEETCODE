692c4357bd97548ce5eaa5c205f5f1cbbe0756e0
THANGETI SWAMY <ugs23128_aids.swamy@cbit.org.in>
2026-08-08 16:04:45 +0530

    Time: 3 ms (83.04%), Space: 22.3 MB (78.02%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,42 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        if not board:
+            return
+        n = len(board)
+        m = len(board[0])
+        queue = deque()
+        for i in range(n):
+            if board[i][0] == "O":
+                queue.append((i, 0))
+                board[i][0] = "#"
+            if board[i][m - 1] == "O":
+                queue.append((i, m - 1))
+                board[i][m - 1] = "#"
+        for j in range(m):
+            if board[0][j] == "O":
+                queue.append((0, j))
+                board[0][j] = "#"
+            if board[n - 1][j] == "O":
+                queue.append((n - 1, j))
+                board[n - 1][j] = "#"
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        while queue:
+            r, c = queue.popleft()
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+                if (0 <= nr < n and
+                    0 <= nc < m and
+                    board[nr][nc] == "O"):
+                    board[nr][nc] = "#"
+                    queue.append((nr, nc))
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                elif board[i][j] == "#":
+                    board[i][j] = "O"

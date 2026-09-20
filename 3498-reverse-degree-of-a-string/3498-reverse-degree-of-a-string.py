e12642aa29a232b54dbdce1e212fe1649434e5e8
THANGETI SWAMY <ugs23128_aids.swamy@cbit.org.in>
2026-09-20 23:30:20 +0530

    Time: 7 ms (70%), Space: 19.3 MB (19.13%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        ans = 0
+        for i in range(len(s)):
+            value = ord('z') - ord(s[i]) + 1
+            ans += value * (i + 1)
+        return ans
